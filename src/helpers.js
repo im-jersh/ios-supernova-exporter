1a19412eb6352e1a2383fd3fa6bcf1b30d4743ca
JERSH <joshua.osteen@trackvia.com>
2023-03-22 15:32:00 -0600

    updating JS helper functions

---
+++ src/helpers.js
@@ -0,0 +1,121 @@
+/**
+ * 
+ * @param {string} text 
+ * @param {string} indentationString 
+ * 
+ * @returns {string}
+ */
+ function createDocumentationComment(text, indentationString) {
+    // Add the [indentationString] to all but the first line
+    return text.trim().split("\n").map((line, index) => ((index > 0) ? `${indentationString}` : ``) + `/// ${line}`).join("\n")
+}
+
+Pulsar.registerFunction("createDocumentationComment", createDocumentationComment)
+
+/**
+ * Convert group name, token name and possible prefix into camelCased string, joining everything together
+ */
+Pulsar.registerFunction(
+    "readableVariableName",
+    function (token, tokenGroup, prefix) {
+      // Create array with all path segments and token name at the end
+      const segments = [...tokenGroup.path];
+      if (!tokenGroup.isRoot) {
+        segments.push(tokenGroup.name)
+      }
+      segments.push(token.name);
+  
+      if (prefix && prefix.length > 0) {
+        segments.unshift(prefix);
+      }
+  
+      // Create "sentence" separated by spaces so we can camelcase it all
+      let sentence = segments.join(" ");
+  
+      // Return camelcased string from all segments
+       sentence = sentence
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+  
+      // only allow letters, digits, underscore, and dollar sign
+      sentence = sentence.replace(/[^a-zA-Z0-9_\$]/g, '_')
+  
+      // prepend underscore if it starts with digit 
+      if (/^\d/.test(sentence)) {
+        sentence = '_' + sentence;
+      }
+      
+      return sentence;
+    }
+  );
+  
+  /**
+   * Behavior configuration of the exporter
+   * Prefixes: Add prefix for each category of the tokens. For example, all colors can start with "color, if needed"
+   */
+  const BEHAVIOR = {
+    color: {
+      fileName: "colors", // this should be somehow synced with output.json contents
+      varName: "Colors",
+      themeProperty: "colors",
+      tokenPrefix: "",
+    },
+    border: {
+      fileName: "borderWidths", // this should be somehow synced with output.json contents
+      varName: "Borders",
+      themeProperty: "borderWidths",
+      tokenPrefix: "",
+    },
+    gradient: {
+      fileName: "gradients", // this should be somehow synced with output.json contents
+      varName: "Gradients",
+      themeProperty: "gradients",
+      tokenPrefix: "",
+    },
+    measure: {
+      fileName: "measures", // this should be somehow synced with output.json contents
+      varName: "Measures",
+      themeProperty: "measures",
+      tokenPrefix: "",
+    },
+  
+    shadow: {
+      fileName: "shadows", // this should be somehow synced with output.json contents
+      varName: "Shadows",
+      themeProperty: "shadows",
+      tokenPrefix: "",
+    },
+    typography: {
+      fileName: "typography", // this should be somehow synced with output.json contents
+      varName: "Typographies",
+      themeProperty: "typographies",
+      tokenPrefix: "",
+    },
+    radius: {
+      fileName: "radii", // this should be somehow synced with output.json contents
+      varName: "Raddii",
+      themeProperty: "radii",
+      tokenPrefix: "",
+    },
+    unknown: {
+      fileName: "uknown",
+      varName: "Unknowns",
+      themeProperty: "unknowns",
+      tokenPrefix: "",
+    }
+  };
+  
+  Pulsar.registerFunction("getBehavior", function (tokenType) {
+    return BEHAVIOR[tokenType.toLowerCase()] || BEHAVIOR['unknown'];
+  });
+  
+  Pulsar.registerFunction("buildReferenceMeta", function(tokenType, tokenValue){
+    return {
+      tokenType,
+      referencedToken: tokenValue.referencedToken
+    }
+  })
+  
+  Pulsar.registerFunction("hasReference", function(tokenValue){
+    return tokenValue.referencedToken !== undefined;
+  })
